81b37f2fe7fa72d02b21d836a87b159a0f656184
UMERZAFAR777 <umerzafar966@gmail.com>
2025-03-03 11:46:08 +0500

    cart_work

--- mysite/urls.py
+++ mysite/urls.py
@@ -48,6 +48,20 @@ urlpatterns = [
     path('shop/filter-data',views.filter_data,name="filter-data"),
 
 
+    path('wishlist/',views.wishlist,name='wishlist'),
+    # path('cart/',views.cart,name='cart'),
+
+
+    path('cart/add/<int:id>/', views.cart_add, name='cart_add'),
+    path('cart/item_clear/<int:id>/', views.item_clear, name='item_clear'),
+    path('cart/item_increment/<int:id>/',
+         views.item_increment, name='item_increment'),
+    path('cart/item_decrement/<int:id>/',
+         views.item_decrement, name='item_decrement'),
+    path('cart/cart_clear/', views.cart_clear, name='cart_clear'),
+    path('cart/cart-detail/',views.cart_detail,name='cart_detail'),
+
+
 
 
 
--- mysite/views.py
+++ mysite/views.py
@@ -220,7 +220,64 @@ def filter_data(request):
 
 
 
+def wishlist(request):
+    return render (request,'wishlist.html')
 
 
+def cart(request):
+    return render (request,'cart.html')
+
+
+from django.contrib.auth.decorators import login_required
+from cart.cart import Cart
+
+
+@login_required(login_url="/accounts/login/")
+def cart_add(request, id):
+    cart = Cart(request)
+    product = Product.objects.get(id=id)
+    cart.add(product=product)
+    return redirect("cart_detail")
+
+
+@login_required(login_url="/accounts/login/")
+def item_clear(request, id):
+    cart = Cart(request)
+    product = Product.objects.get(id=id)
+    cart.remove(product)
+    return redirect("cart_detail")
+
+
+@login_required(login_url="/accounts/login/")
+def item_increment(request, id):
+    cart = Cart(request)
+    product = Product.objects.get(id=id)
+    cart.add(product=product)
+    return redirect("cart_detail")
+
+
+@login_required(login_url="/accounts/login/")
+def item_decrement(request, id):
+    cart = Cart(request)
+    product = Product.objects.get(id=id)
+
+    # Check if the product is in the cart and its quantity is greater than 1
+    if str(product.id) in cart.cart and cart.cart[str(product.id)]["quantity"] > 1:
+        cart.decrement(product=product)
+
+    return redirect("cart_detail")
+
+
+
+@login_required(login_url="/accounts/login/")
+def cart_clear(request):
+    cart = Cart(request)
+    cart.clear()
+    return redirect("cart_detail")
+
+
+@login_required(login_url="/accounts/login/")
+def cart_detail(request):
+    return render(request, 'cart/cart_detail.html')
 
 
